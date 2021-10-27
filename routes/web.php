51779cdcbfeb3c5a209ac6e65a6c719fadb59042
Muhammad Zeshan <zeshan77@gmail.com>
2021-10-27 08:30:46 +0500

    run migrations by force when in tests

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('pages.home');
 });
 
 Route::get('/contact', [PagesController::class, 'contact']);
